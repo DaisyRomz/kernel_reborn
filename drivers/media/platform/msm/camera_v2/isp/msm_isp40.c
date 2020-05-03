d1372c60873b9482db2fa2b82ebfc52aaf94ee43
Albert I <kras@raphielgang.org>
2020-05-04 03:14:35 +0800

    [TEMP] treewide: Update indentations to fix compiler warnings
    This is a temporary commit and needs to be split into few commits.
    
    Signed-off-by: Albert I <kras@raphielgang.org>
    Signed-off-by: Aarqw12 <lcockx@protonmail.com>

--- drivers/media/platform/msm/camera_v2/isp/msm_isp40.c
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp40.c
@@ -219,7 +219,7 @@ static int32_t msm_vfe40_init_qos_parms(struct vfe_device *vfe_dev,
 		if (rc < 0) {
 			pr_err("%s: NO D/S register info\n", __func__);
 			kfree(ds_settings);
-			kfree(ds_regs);
+			   kfree(ds_regs);
 		} else {
 			if (ds_parms->settings) {
 				rc = of_property_read_u32_array(of_node,
@@ -234,8 +234,8 @@ static int32_t msm_vfe40_init_qos_parms(struct vfe_device *vfe_dev,
 					for (i = 0; i < ds_entries; i++)
 						msm_camera_io_w(ds_settings[i],
 							vfebase + ds_regs[i]);
-						kfree(ds_regs);
-						kfree(ds_settings);
+						      kfree(ds_regs);
+						        kfree(ds_settings);
 				}
 			} else {
 				kfree(ds_regs);
